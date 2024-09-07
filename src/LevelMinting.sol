49c5e46ab8b75b4b54f0577ea116259d39e3777a
alh99 <55510932+alh99@users.noreply.github.com>
2024-09-06 20:15:48 -0400

    edit

--- src/LevelMinting.sol
+++ src/LevelMinting.sol
@@ -6,7 +6,7 @@ pragma solidity >=0.8.19;
  */
 
 import "./SingleAdminAccessControl.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
--- src/LevelReserveManager.sol
+++ src/LevelReserveManager.sol
@@ -9,7 +9,7 @@ import "./interfaces/ILevelMinting.sol";
 import "./interfaces/IStakedlvlUSD.sol";
 import "./interfaces/ISymbioticVault.sol";
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
